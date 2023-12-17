c0ea23190087b64379044b1706e422d3c4e8998c
Sutham Rojanusorn <>
2023-12-17 19:13:52 +0700

    Add toString to Class

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,9 @@
 class HelloWorld:
   def __init__(self, name):
     self.name = name
+  def __str__(self):
+    return f"Hello {self.name}"
 
 hello = HelloWorld("Sutham")
 print("Hello", hello.name)
+print(hello)
